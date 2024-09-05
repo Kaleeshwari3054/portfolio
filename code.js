af7902c853ba6ea34f25e80cd4d3fa36715b7781
Kaleeshwari3054 <kaleeshwari894@gmail.com>
2024-09-05 14:24:50 +0530

    portfolio changes

--- code.js
+++ code.js
@@ -1,7 +1,96 @@
+// let mark=44;
+// if(mark>=60){
+//     console.log("pass")
+// }
+// else if(mark>=45){
+//     console.log("pass2")   
+// }
+// else{
+//     console.log("fail")
+// }
 
 
-let P=5000;
-let R=6;
-let T=4;
-let CompoundInterest = P * (1 + R/100)^T - P;
-console.log("The compound interest at the end of the period;",CompoundInterest);
+// if (age >= 5 && age <= 12)
+
+
+// let mark= 64;
+// let grade ;
+// if(mark>=60){
+//     console.log("pass")
+//     grade="A";
+// }
+// else if(mark>=45){
+//     console.log("pass2")  
+//     grade= "B";
+// }
+// else{
+//     console.log("fail")
+//     grade= "no grade";
+// }
+// console.log("Grade:" , grade)
+// let day = 3;
+// switch (day) {
+//     case 1:
+//         console.log("Today is Monday");
+//         break;
+//     case 2:
+//         console.log("Today is Tuesday");
+//         break;
+//     case 3:
+//         console.log("Today is Wednesday");
+//         break;
+//     case 4:
+//         console.log("Today is Thursday");
+//         break;
+//     case 5:
+//         console.log("Today is Friday");
+//         break;
+//     case 6:
+//         console.log("Today is Saturday");
+//         break;
+//     case 7:
+//         console.log("Today is Sunday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+//         break;
+// }
+// let status=on;
+
+// case on:
+//     console.log("power on");
+//     break;
+
+
+// let n="i" ;
+// if(n=="a"|| n=="e" || n=="i" || n=="o" || n=="u"){
+//     console.log("it is a vowel letter")
+// }
+// else {
+//   console.log("it is not vowel word")
+// }
+// let age=20;
+// let price=20;
+// if(age>5){
+//     console.log("Free")
+
+// }
+// else if(age > 5 && age <= 12){
+//     console.log("10")
+// }
+// else if(age>13 && age <= 64){
+//     console.log("20")
+// }
+// else{
+//     console.log("15")
+// }
+
+let a=26;
+let b=30;
+if(a<b){
+    console.log("smallest number")
+}
+else{
+    console.log("greatest number")
+}
+
